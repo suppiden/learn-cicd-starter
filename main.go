3a612af7a6b1eefcb3754f6f27b3cc8c86c2cc38
suppiden <jonathansantos.vae@gmail.com>
2026-05-07 15:06:35 +0200

    failure of static check2

--- main.go
+++ main.go
@@ -22,9 +22,10 @@ type apiConfig struct {
 }
 
 func unused() {
-    // this function does nothing
-    // and is called nowhere
+	// this function does nothing
+	// and is called nowhere
 }
+
 //go:embed static/*
 var staticFiles embed.FS
 
